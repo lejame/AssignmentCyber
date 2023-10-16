03cb27141bc636d990960cda1a4f88b9397c402f
Lê Hữu Lợi <121961941+lejame@users.noreply.github.com>
2023-10-16 18:29:16 +0700

    update

--- BT09/Main.java
+++ BT09/Main.java
@@ -1,6 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] agrs) {
+        input();
+    }
 
+    /**
+     * 
+     */
+    public static void input() {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = { 23, 1, 5, 22, 6, 8, 9, -3 };
+        System.out.print("Nhập lựa chọn của bạn:");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1: {
+                System.out.println("Result:" + cauB(arr));
+                break;
+            }
+            case 2: {
+                System.out.println("Result:" + cauC(arr));
+                break;
+            }
+            case 3: {
+                System.out.println("Result:" + cauD(arr));
+                break;
+            }
+            case 4: {
+                System.out.println("Result:" + cauE(arr));
+                break;
+            }
+            case 5: {
+                System.out.println("Result:" + cauF(arr));
+                break;
+            }
+            case 6: {
+                System.out.println("Result:" + cauG(arr));
+                break;
+            }
+            case 7: {
+                System.out.println("Result:" + cauH(arr));
+                break;
+            }
+            case 8: {
+                System.out.println("Result:" + cauI(arr));
+                break;
+            }
+            case 9: {
+                System.out.println("Result:" + cauJ(arr));
+                break;
+            }
+            case 10: {
+                System.out.println("Result:" + cauK(arr));
+                break;
+            }
+            default : System.out.print("Nhập lại lựa chọn của bản");
+        }
     }
 
     public static int cauB(int[] arr) {
@@ -54,14 +109,14 @@ public class Main {
     }
 
     public static int cauG(int[] arr) {
+        if (!checkEven(arr)) {
+            return -1;
+        }
         int numberEven = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 numberEven = arr[i];
             }
-        }
-        if (numberEven == 0) {
-            return -1;
         }
         return numberEven;
     }
@@ -74,7 +129,75 @@ public class Main {
                 break;
             }
         }
-        if(numberEven!=0){return numberEven;}
+        if (numberEven != 0) {
+            return numberEven;
+        }
         return -1;
+    }
+
+    public static int cauI(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (checkPrime(arr[i]) == 1) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    public static int cauJ(int[] arr) {
+        if (!checkPositivenumber(arr)) {
+            return -1;
+        }
+        int positivenumber = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positivenumber = arr[i];
+            }
+        }
+        return positivenumber;
+    }
+
+    public static int cauK(int[] arr) {
+        if (!checkEven(arr)) {
+            return -1;
+        }
+        int positivenumber = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0 && arr[i] < positivenumber) {
+                positivenumber = arr[i];
+            }
+        }
+        return positivenumber;
+    }
+
+    public static int checkPrime(int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static boolean checkEven(int[] arr) {
+        for (int a : arr) {
+            if (a % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkPositivenumber(int[] arr) {
+        for (int a : arr) {
+            if (a >= 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
