7fb488b2c5902ed22a389da237a6424161505bec
Lê Hữu Lợi <121961941+lejame@users.noreply.github.com>
2024-01-03 23:56:46 +0700

    commit

--- SPRINGBOOT/BT_3_1_2024/Management/Management/src/main/java/BT_20_12/Management/Controller/UsersController.java
+++ SPRINGBOOT/BT_3_1_2024/Management/Management/src/main/java/BT_20_12/Management/Controller/UsersController.java
@@ -3,6 +3,7 @@ package BT_20_12.Management.Controller;
 import BT_20_12.Management.Entity.UsersEntity;
 import BT_20_12.Management.Services.UsersService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,26 @@ public class UsersController {
     private UsersService usersService;
 
     @GetMapping("/login")
-    public String login(HttpServletResponse httpServletResponse, Model model) {
+    public String login(HttpServletRequest request, Model model) {
         model.addAttribute("error","");
-        Cookie cookie = new Cookie("hello", URLEncoder.encode("dang nhap di", StandardCharsets.UTF_8));
-
-        httpServletResponse.addCookie(cookie);
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null){
+            return "login";
+        }
+        String email = "";
+        String pass = "";
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("email")){
+                email = cookie.getValue();
+            }
+            if(cookie.getName().equals("password")){
+                pass = cookie.getValue();
+            }
+        }
+        model.addAttribute("email",email);
+        model.addAttribute("password",pass);
         return "login";
+
     }
     @GetMapping("/login_error")
     public String login_error(Model model){
@@ -35,11 +50,17 @@ public class UsersController {
         return "login";
     }
     @PostMapping("/login_action")
-    public RedirectView form_login(HttpSession httpSession, @RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+    public RedirectView form_login(HttpServletResponse httpServletResponse,HttpSession httpSession, @RequestParam("email") String email, @RequestParam("password") String password, Model model) {
         UsersEntity user = usersService.getUsersEntityByEmailAndAndPassword(email, password);
         if (user == null) {
             return new RedirectView("/login_error");
         }
+        Cookie cookie_email = new Cookie("email",email);
+        Cookie cookie_password = new Cookie("password",password);
+
+        httpServletResponse.addCookie(cookie_email);
+        httpServletResponse.addCookie(cookie_password);
+
         return new RedirectView("/home");
     }
 
